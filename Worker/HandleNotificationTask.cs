7c5e7c9dd0516cc96a9187bb08a07509b32f6259
ThereFox <alexandrgrizunow@gmail.com>
2024-07-28 19:41:07 +0300

    declarete task and shedule

---
+++ Worker/HandleNotificationTask.cs
@@ -0,0 +1,36 @@
+﻿using Application;
+using Coravel.Invocable;
+using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    public class HandleNotificationTask : IInvocable
+    {
+        private readonly NotificationSendService _service;
+
+        public HandleNotificationTask(NotificationSendService service)
+        {
+            _service = service;
+        }
+
+        public async Task Invoke()
+        {
+            while (true)
+            {
+                var handleNotificationResult = await _service.HandleNotification();
+
+                if (handleNotificationResult.IsFailure)
+                {
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Worker/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Notificator.Persistense.DI;
+using Microsoft.Extensions.Hosting;
+using Coravel;
+
+namespace Worker
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<PersistenseModule>();
+
+            var container = builder.Build();
+
+            container.Resolve<IHost>().Services.UseScheduler(
+                sheduler => sheduler.Schedule<HandleNotificationTask>()
+                .EveryThirtyMinutes()
+                );
+        }
+    }
+}
