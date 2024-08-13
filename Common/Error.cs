eb851548c6be5c66881802317e83dfc6ea5bbbae
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-08-13 20:34:24 +0300

    Поправил проекты

--- Common/Error.cs
+++ Common/Error.cs
@@ -1,8 +1,21 @@
-﻿namespace Common
+﻿using CSharpFunctionalExtensions;
+
+namespace Common
 {
     public record Error
     (
         string ErrorMessage
     );
 
+    public static class AsResultClass
+    {
+        public static Result AsResult(this Error error)
+        {
+            return Result.Failure(error.ErrorMessage);
+        }
+        public static Result<T> AsResult<T>(this Error error)
+        {
+            return Result.Failure<T>(error.ErrorMessage);
+        }
+    }
 }
