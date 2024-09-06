e68fcd91fa2f211c47e4389927b1b21a50502d45
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-09-06 06:23:55 +0400

    Добавил методы в интерфейс логирования

---
+++ Application/Interfaces/Services/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Domain.Entitys;
+
+namespace Application.Interfaces.Services
+{
+    public interface ILogger
+    {
+        public Task LogRequest(Guid bluepringId, Guid customerId);
+        public Task LogSendMessage(Notification message);
+        public Task LogError(Error error);
+        public Task LogAddToQueue(Notification message);
+    }
+}
