36201ae4198c40d4e06d3e10da0057ec34098e76
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-09-06 06:26:08 +0400

    Реализовал создание сообщений по команде и добавил VO в индекс

---
+++ Application/Services/NotificationValidator.cs
@@ -0,0 +1,64 @@
+﻿using App.Stores;
+using CSharpFunctionalExtensions;
+using Domain.Entitys;
+using Domain.ValueObject;
+using Infrastructure.Kafka.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class NotificationCreator
+    {
+        private readonly ICustomerStore _customerStore;
+        private readonly IBlueprintStore _blueprintStore;
+
+        public async Task<Result<Notification>> CreateByInfo(SendNotificationInputObject request)
+        {
+            var blueprintCreateResult = await CreateBlueprint(request.BlueprintId);
+
+            if (blueprintCreateResult.IsFailure)
+            {
+                return blueprintCreateResult.ConvertFailure<Notification>();
+            }
+
+            var getDeviceResult = await GetDeviceByCustomer(request.CustomerId, blueprintCreateResult.Value.Channel);
+
+            if (getDeviceResult.IsFailure)
+            {
+                return getDeviceResult.ConvertFailure<Notification>();
+            }
+
+            var result = Notification.Create(
+                Guid.NewGuid(),
+                getDeviceResult.Value,
+                blueprintCreateResult.Value,
+                NotificationStatus.Created,
+                DateTime.Now,
+                default);
+
+            return result;
+        }
+        protected async Task<Result<Device>> GetDeviceByCustomer(Guid CustomerId, NotificationChannel channel)
+        {
+            var getCustomerResult = await _customerStore.Get(CustomerId);
+
+            if (getCustomerResult.IsFailure)
+            {
+                return Result.Failure<Device>("Cant get customer");
+            }
+
+            var pickDeviceResult = getCustomerResult.Value.PickDeviceForNotificationByChannel(channel);
+
+            return pickDeviceResult;
+        }
+
+        protected async Task<Result<Blueprint>> CreateBlueprint(Guid BlueprintId)
+        {
+            return await _blueprintStore.Get(BlueprintId);
+        }
+    }
+}
---
+++ Domain/ValueObject/BlueprintTemplate.cs
@@ -0,0 +1,40 @@
+﻿using CSharpFunctionalExtensions;
+using Domain.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObject
+{
+    public class BlueprintTemplate : CSharpFunctionalExtensions.ValueObject
+    {
+        private string _template { get; set; }
+
+        public string FormatMessageForCustomer(Customer customer)
+        {
+            return _template;
+        }
+
+        public static Result<BlueprintTemplate> Create(string template)
+        {
+            if (string.IsNullOrWhiteSpace(template))
+            {
+                return Result.Failure<BlueprintTemplate>("template must be not null and not empty");
+            }
+
+            return Result.Success(new BlueprintTemplate(template));
+        }
+
+        protected BlueprintTemplate(string Template)
+        {
+            _template = Template;
+        }
+
+        protected override IEnumerable<IComparable> GetEqualityComponents()
+        {
+            yield return _template;
+        }
+    }
+}
