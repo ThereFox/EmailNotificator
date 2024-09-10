26d0e0f12f0e780aeeb9cdf6dd31b101714b2ddc
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-09-10 20:31:38 +0400

    Добавил данные в БД по умолчанию

--- Notificator.Persistense/ApplicationDBContext.cs
+++ Notificator.Persistense/ApplicationDBContext.cs
@@ -14,11 +14,45 @@ public class ApplicationDBContext : DbContext
     
     public ApplicationDBContext(DbContextOptions<ApplicationDBContext> DBconfiguration) : base(DBconfiguration)
     {
+        this.Database.EnsureCreated();
     }
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDBContext).Assembly);
+
+
+        var baseCustomer = new CustomerEntity()
+        {
+            Id = Guid.Parse("a7f1cf1f-5f4f-4159-99cc-80a4e9f7c5cb"),
+            CreatedAt = DateTime.MinValue,
+            Role = CustomerRole.Base.Value,
+            UserName = "ThereFox"
+        };
+        var baseCustomerDevice = new DeviceSettingsEntity()
+        {
+            IsActive = true,
+            CreatedAt = DateTime.MinValue,
+            NotificationChannel = NotificationChannel.Email.Value,
+            Id = Guid.NewGuid(),
+            UpdatedAt = DateTime.MinValue,
+            CustomerId = baseCustomer.Id
+        };
+
+        var hellowBlueprint = new NotificationBlueprintEntity()
+        {
+            Id = Guid.Parse("b7f1cf1f-5f4f-4159-99cc-80a4e9f7c5cb"),
+            Channel = NotificationChannel.Email.Value,
+            CreatedAt = DateTime.MinValue,
+            Content = "Test content"
+        };
+
+
+        modelBuilder.Entity<CustomerEntity>().HasData(baseCustomer);
+        modelBuilder.Entity<DeviceSettingsEntity>().HasData(baseCustomerDevice);
+        modelBuilder.Entity<NotificationBlueprintEntity>().HasData(hellowBlueprint);
+
+
     }
-    
+
 }
