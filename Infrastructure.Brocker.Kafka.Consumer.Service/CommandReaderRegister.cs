86cbe5adda755a56d6bd9468c05d8e5c661664bf
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-09-10 20:29:01 +0400

    Заменил задачу шедулера hostedService

---
+++ Infrastructure.Brocker.Kafka.Consumer.Service/CommandReaderRegister.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Brocker.Kafka.Consumer.Service
+{
+    public static class CommandReaderRegister
+    {
+        public static IServiceCollection AddCommandReaderService(this IServiceCollection services)
+        {
+            services.AddHostedService<CommandReaderService>();
+
+            return services;
+        }
+    }
+}
---
+++ Infrastructure.Brocker.Kafka.Consumer.Service/CommandReaderService.cs
@@ -0,0 +1,50 @@
+﻿using Application;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Brocker.Kafka.Consumer.Service
+{
+    public class CommandReaderService : IHostedService
+    {
+        private readonly NotificationSendService _service;
+        private CancellationTokenSource _cancelerTokenSource = new();
+
+        public CommandReaderService(NotificationSendService service)
+        {
+            _service = service;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Factory.StartNew(consumeLoop, _cancelerTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+            return Task.CompletedTask;
+        }
+
+        private async Task consumeLoop()
+        {
+            while (_cancelerTokenSource.IsCancellationRequested == false)
+            {
+                await handleMessages();
+            }
+        }
+
+        private async Task handleMessages()
+        {
+            var handleNotificationResult = await _service.HandleNotification();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_cancelerTokenSource.IsCancellationRequested)
+            {
+                return;
+            }
+
+            await _cancelerTokenSource.CancelAsync();
+        }
+    }
+}
